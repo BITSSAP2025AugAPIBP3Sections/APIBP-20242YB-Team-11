cd04bfcfc9d5ca833332f21a5986611606b3604e
I528989 <bhaskar.kulshrestha@sap.com>
2025-11-06 22:56:41 +0530

    routing method for product microservice

--- routes/Product.js
+++ routes/Product.js
@@ -10,3 +10,18 @@ const {
   getOffersByFilter,
   searchOffers,
 } = require("../controllers/Offers");
+const { protect, authorize } = require("../middleware/authMiddleware");
+
+// Retailer routes
+router.post("/", protect, authorize("retailer"), createOffer);
+router.put("/:id", protect, authorize("retailer"), updateOffer);
+router.delete("/:id", protect, authorize("retailer"), deleteOffer);
+router.get("/mine", protect, authorize("retailer"), getMyOffers);
+
+// Public routes
+router.get("/", getAllOffers);
+router.get("/filter", getOffersByFilter);
+router.get("/search", searchOffers);
+router.get("/:id", getOfferById);
+
+module.exports = router;
