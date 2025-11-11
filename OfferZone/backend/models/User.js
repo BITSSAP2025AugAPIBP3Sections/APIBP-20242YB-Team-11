253c5cc8c1edc8e3aef1826d1fa67f5c3c746a50
I528989 <bhaskar.kulshrestha@sap.com>
2025-11-12 00:15:22 +0530

    made the routes for the model services

--- OfferZone/backend/models/User.js
+++ OfferZone/backend/models/User.js
@@ -2,3 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { signup, login, getCurrentUser } = require("../controllers/User");
 const { protect } = require("../middleware/authMiddleware");
+
+router.post("/", signupservice); 
+router.post("/session", loginservice); 
+router.get("/me", protect, getCurrentUserservice); 
