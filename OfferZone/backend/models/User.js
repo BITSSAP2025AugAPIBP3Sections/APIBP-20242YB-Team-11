07f3a9c7a6f7f0a29152d0cf710424fabfc7fed1
I528989 <bhaskar.kulshrestha@sap.com>
2025-11-12 00:21:07 +0530

    rectified the route method

--- OfferZone/backend/models/User.js
+++ OfferZone/backend/models/User.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { signup, login, getCurrentUser } = require("../controllers/User");
 const { protect } = require("../middleware/authMiddleware");
 
-router.post("/", signupservice); 
-router.post("/session", loginservice); 
-router.get("/me", protect, getCurrentUserservice); 
+router.post("/", signup); 
+router.post("/session", login); 
+router.get("/me", protect, getCurrentUser); 
