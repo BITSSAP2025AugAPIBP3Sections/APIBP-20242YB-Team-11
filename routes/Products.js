a27e488659555bbad1e28c86bf9d4e94dd559757
Aditi-Rai <raiaditi0729@gmail.com>
2025-11-19 23:43:07 +0530

    Adding product routes

---
+++ routes/Products.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const {
+  createProduct,
+  getAllProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+  getMyProducts,
+} = require("../controllers/Products");
+const { protect, authorize } = require("../middleware/authMiddleware");
+
+router.post("/", protect, authorize("retailer"), createProduct);
+router.get("/", getAllProducts);
+router.get("/mine", protect, authorize("retailer"), getMyProducts);
+router.get("/:id", getProductById);
+router.put("/:id", protect, authorize("retailer"), updateProduct);
+router.delete("/:id", protect, authorize("retailer"), deleteProduct);
+
+module.exports = router;
